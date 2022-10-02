87ef95652097453c4bf47a3bd750227d3c674e21
dnjoe96 <donjoedbest@gmail.com>
2022-10-03 00:09:38 +0100

    Set up Auto scaler

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+#kubectl apply -f aws-configmap.yaml
+#kubectl apply -f aws-secret.yaml
+#kubectl apply -f env-configmap.yaml
+
 kubectl delete deployment reverseproxy
 kubectl delete deployment backend-user
 kubectl delete deployment backend-feed
@@ -17,3 +21,7 @@ kubectl apply -f reverseproxy-deployment.yaml
 kubectl apply -f backend-feed-deployment.yaml
 kubectl apply -f backend-user-deployment.yaml
 kubectl apply -f frontend-deployment.yaml
+
+kubectl autoscale deployment backend-user --cpu-percent=50 --min=1 --max=10
+kubectl autoscale deployment backend-feed --cpu-percent=50 --min=1 --max=10
+kubectl autoscale deployment udagram-frontend --cpu-percent=50 --min=1 --max=10
